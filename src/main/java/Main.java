7154cc628453ffd2d2614f053c0843c0f5152f2b
chye0001 <chye0001@stud.kea.dk>
2023-09-25 13:18:08 +0200

    Added: test 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("input 2 tal: ");
+        System.out.print("tal 1: ");
+        int a = scanner.nextInt();
+        System.out.println("tal 2: ");
+        int b = scanner.nextInt();
+
         System.out.println();
-        int A = 1000;
-        int b = 12;
-        System.out.println(A-b);
+
+        System.out.println("resultat: " + a+b);
+
+
     }
 }

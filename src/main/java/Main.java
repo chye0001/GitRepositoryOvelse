55fa99488cb7d959c120a3ae2ab0a7133a474d42
chye0001 <chye0001@stud.kea.dk>
2023-09-25 13:13:57 +0200

    Added: test 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
         int A = 1000;
-        System.out.println(A);
+        int b = 12;
+        System.out.println(A-b);
     }
 }
